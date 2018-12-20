7d9581fad7eeed9a6cb88379313296f21561e09d
YangXiang <yangxiang@chuangjialive.com>
2018-12-20 17:28:14 +0800

    设计模式-模板模式

---
+++ src/main/java/com/yang/design/mode/template/Tea.java
@@ -0,0 +1,17 @@
+package com.yang.design.mode.template;
+
+/**
+ * 茶和咖啡都是继承自咖啡因饮料
+ */
+public class Tea extends CaffeineBeverage {
+
+    @Override
+    void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+}
