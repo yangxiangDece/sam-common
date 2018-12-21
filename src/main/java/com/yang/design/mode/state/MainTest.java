7d2532828de343e00f3bb1472e0fced5abf7bddd
YangXiang <yangxiang@chuangjialive.com>
2018-12-21 15:53:45 +0800

    设计模式-状态模式

---
+++ src/main/java/com/yang/design/mode/state/MainTest.java
@@ -0,0 +1,22 @@
+package com.yang.design.mode.state;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        GumballMachine gumballMachine=new GumballMachine(5);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+    }
+}
---
+++ src/main/java/com/yang/design/mode/state/WinnerState.java
@@ -0,0 +1,45 @@
+package com.yang.design.mode.state;
+
+public class WinnerState implements State {
+
+    private GumballMachine gumballMachine;
+
+    public WinnerState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("Please wait, we're already giving you a gumball");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Sorry, you already turned the crank");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("Turning twice doesn't get you another gumball");
+    }
+
+    @Override
+    public void dispense() {
+        //需要放出两颗糖果
+        System.out.println("You're a WINNER! You get two gumball for your quarter");
+        //放出第一颗糖果
+        gumballMachine.releaseBall();
+        if (gumballMachine.getCount()==0) {
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        } else {
+            //放出第二颗糖果
+            gumballMachine.releaseBall();
+            if (gumballMachine.getCount()>0) {
+                gumballMachine.setState(gumballMachine.getNoQuarterState());
+            } else {
+                System.out.println("Oops, out of gumballs");
+                gumballMachine.setState(gumballMachine.getSoldOutState());
+            }
+        }
+    }
+}
