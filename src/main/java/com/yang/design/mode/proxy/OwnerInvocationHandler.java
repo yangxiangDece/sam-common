4329dc3aa5a11b8ae94e6ac07b1c6cb3ca400758
YangXiang <yangxiang@chuangjialive.com>
2018-12-24 11:34:09 +0800

    设计模式-代理模式

---
+++ src/main/java/com/yang/design/mode/proxy/OwnerInvocationHandler.java
@@ -0,0 +1,39 @@
+package com.yang.design.mode.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+/**
+ * 动态代理：运行时才将类创建出来，代码开始执行时，还没有proxy类，它是根据需要从你传入的接口集创建的
+ *      通过代理类的一个静态方法Proxy.isProxyClass(personBean.getClass())判断是否是一个代理类，是则返回true
+ */
+public class OwnerInvocationHandler implements InvocationHandler {
+
+    private PersonBean person;
+
+    private OwnerInvocationHandler(PersonBean personBean) {
+        this.person = personBean;
+    }
+
+    public static PersonBean getOwnerProxy(PersonBean person){
+        return (PersonBean) Proxy.newProxyInstance(person.getClass().getClassLoader(), person.getClass().getInterfaces(), new OwnerInvocationHandler(person));
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            String methodName=method.getName();
+            if(methodName.startsWith("get") || methodName.startsWith("set")){
+                System.out.println("执行方法："+methodName+", 参数："+ Arrays.asList(args)+", proxy："+proxy);
+                return method.invoke(person,args);
+            } else if (methodName.equals("setHotOrNotRating")){
+                return new IllegalAccessException();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
