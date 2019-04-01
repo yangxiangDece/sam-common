47ac5b5f132f1d6e75521cac63716a170a9bef61
YangXiang <yangxiang@chuangjialive.com>
2019-04-01 17:55:36 +0800

    数据结构与算法

---
+++ src/main/java/com/yang/arithmetic/sort/BucketSort.java
@@ -0,0 +1,45 @@
+package com.yang.arithmetic.sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 桶排序
+ */
+public class BucketSort {
+
+    public static void main(String[] args) {
+        int[] array = {12,66,222,141,38,65,97,15,21,62,111,98,54,56,17,18,23,34,15,35,25,53,51};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void sort(int[] arr){
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+
+        //桶数
+        int bucketNum = (max - min) / arr.length + 1;
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>(bucketNum);
+        for(int i = 0; i < bucketNum; i++){
+            bucketArr.add(new ArrayList<>());
+        }
+
+        //将每个元素放入桶
+        for(int i = 0; i < arr.length; i++){
+            int num = (arr[i] - min) / (arr.length);
+            bucketArr.get(num).add(arr[i]);
+        }
+
+        //对每个桶进行排序
+        for(int i = 0; i < bucketArr.size(); i++){
+            Collections.sort(bucketArr.get(i));
+        }
+    }
+}
