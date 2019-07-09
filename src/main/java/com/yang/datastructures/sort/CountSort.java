ed9c73bccb14ad5821a8d9c5ab1cbdbc3dcb1dc4
yangxiang <596081561@qq.com>
2019-07-09 20:52:03 +0800

    基数算法

--- src/main/java/com/yang/datastructures/sort/CountSort.java
+++ src/main/java/com/yang/datastructures/sort/CountSort.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 
 /**
  * 计数排序
- *      1、找出待排序的数组中最大和最小的元素；
- *      2、统计数组中每个值为i的元素出现的次数，存入数组C的第i项；
- *      3、对所有的计数累加（从C中的第一个元素开始，每一项和前一项相加）；
- *      4、反向填充目标数组：将每个元素i放在新数组的第C(i)项，每放一个元素就将C(i)减去1。
+ * 1、找出待排序的数组中最大和最小的元素；
+ * 2、统计数组中每个值为i的元素出现的次数，存入数组C的第i项；
+ * 3、对所有的计数累加（从C中的第一个元素开始，每一项和前一项相加）；
+ * 4、反向填充目标数组：将每个元素i放在新数组的第C(i)项，每放一个元素就将C(i)减去1。
  */
 public class CountSort {
 
     public static void main(String[] args) {
-        int[] array = {12,66,222,141,38,65,97,777,88,62,111,98,54,56,17,18,23,34,15,35,25,53,51,1};
+        int[] array = {12, 66, 222, 141, 38, 65, 97, 777, 88, 62, 111, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51, 1};
         sort(array);
         System.out.println(Arrays.toString(array));
     }
 
-    private static void sort(int[] array){
+    private static void sort(int[] array) {
         int bias, min = array[0], max = array[0];
         //找出最大值，最小值，确定数组范围
         for (int i = 1; i < array.length; i++) {
-            max = Math.max(max,array[i]);
-            min = Math.min(min,array[i]);
+            max = Math.max(max, array[i]);
+            min = Math.min(min, array[i]);
         }
         bias = 0 - min;
         int[] temp = new int[max - min + 1];
---
+++ src/main/java/com/yang/datastructures/sort/RadixSort.java
@@ -0,0 +1,71 @@
+package com.yang.datastructures.sort;
+
+import java.util.Arrays;
+
+/**
+ * 基数排序
+ * 基数排序(Radix Sort)是桶排序的扩展，它的基本思想是：将整数按位数切割成不同的数字，然后按每个位数分别比较。
+ * 将所有待比较数值统一为同样的数位长度，数位较短的数前面补零。然后，从最低位开始，依次进行一次排序。
+ * 这样从最低位排序一直到最高位排序完成以后, 数列就变成一个有序序列。
+ */
+public class RadixSort {
+
+    public static void main(String[] args) {
+        int[] arrays = CreateArrays.getArrays(8);
+        System.out.print("before sort：");
+        System.out.println(Arrays.toString(arrays));
+        sort(arrays);
+        System.out.println();
+        System.out.print(" after sort：");
+        System.out.println(Arrays.toString(arrays));
+    }
+
+    /**
+     * 对数组按照"某个位数"进行排序(桶排序)
+     *
+     * @param arrays
+     */
+    private static void sort(int[] arrays) {
+
+        // 找到数组中最大数的位数
+        int max = getMax(arrays);
+        int maxLength = String.valueOf(max).length();
+
+        // 定义一个二维数组，表示10个桶（因为是10进制），每个桶就是一个一位数组 用来存放拆分的数
+        int[][] bucket = new int[10][arrays.length];
+
+        // 定义一个一维数组来记录每个桶中的拆分的数据个数
+        int[] bucketElementCounts = new int[10];
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            // 将数据放入到桶中 第一次个位，第二次十位，百位...
+            for (int j = 0; j < arrays.length; j++) {
+                int di = arrays[j] / n % 10;
+                bucket[di][bucketElementCounts[di]] = arrays[j];
+                bucketElementCounts[di]++;
+            }
+            int index = 0;
+            // 遍历每个桶并且将桶中的数据放入到原数组中
+            for (int k = 0; k < bucket.length; k++) {
+                // 如果桶中有数据，就放入到原数组中
+                if (bucketElementCounts[k] != 0) {
+                    for (int l = 0; l < bucketElementCounts[k]; l++) {
+                        // 将桶中的数据 依次放入到原数组中 即覆盖原数组
+                        arrays[index++] = bucket[k][l];
+                    }
+                }
+                bucketElementCounts[k] = 0;
+            }
+        }
+    }
+
+    private static int getMax(int[] arrays) {
+        int max = arrays[0];
+        for (int i = 0; i < arrays.length; i++) {
+            if (arrays[i] > max) {
+                max = arrays[i];
+            }
+        }
+        return max;
+    }
+}
