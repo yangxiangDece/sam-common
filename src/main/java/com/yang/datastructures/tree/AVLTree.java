032ff05aff29eefd2498621a751b302369149308
yangxiang <596081561@qq.com>
2019-07-26 08:52:19 +0800

    AVL平衡二叉树

--- src/main/java/com/yang/datastructures/tree/AVLTree.java
+++ src/main/java/com/yang/datastructures/tree/AVLTree.java
@@ -1,27 +1,31 @@
 package com.yang.datastructures.tree;
 
 /**
- * 二叉排序树
+ * AVL平衡二叉树
  */
-public class BinarySortTree {
+public class AVLTree {
 
     public static void main(String[] args) {
-        int[] arrays = {7, 3, 10, 12, 5, 1, 9, 0};
-        BinarySortTreeU tree = new BinarySortTreeU();
+//        int[] arrays = {4, 3, 6, 5, 7, 8}; // 左旋转
+//        int[] arrays = {10, 12, 8, 9, 7, 6};// 右旋转
+        int[] arrays = {10, 11, 7, 6, 8, 9};// 双旋转
+        AVLTreeU tree = new AVLTreeU();
         for (int i = 0; i < arrays.length; i++) {
-            tree.add(new BinaryNode(arrays[i]));
+            tree.add(new AVLNode(arrays[i]));
         }
+        System.out.println("中序遍历：");
         tree.infixOrder();
-        tree.delNode(10);
-        System.out.println("删除后：");
-        tree.infixOrder();
+
+        System.out.println("二叉树的高度：" + tree.getRoot().height());
+        System.out.println("二叉树的左子树高度：" + tree.getRoot().leftHeight());
+        System.out.println("二叉树的右子树高度：" + tree.getRoot().rightHeight());
     }
 }
 
-class BinarySortTreeU {
-    private BinaryNode root;
+class AVLTreeU {
+    private AVLNode root;
 
-    public void add(BinaryNode node) {
+    public void add(AVLNode node) {
         if (root == null) {
             root = node;
         } else {
@@ -29,11 +33,15 @@ class BinarySortTreeU {
         }
     }
 
+    public AVLNode getRoot() {
+        return root;
+    }
+
     public void delNode(int value) {
         if (root == null) {
             return;
         } else {
-            BinaryNode targetNode = search(value);
+            AVLNode targetNode = search(value);
             if (targetNode == null) {
                 return;
             }
@@ -42,7 +50,7 @@ class BinarySortTreeU {
                 root = null;
                 return;
             }
-            BinaryNode parent = searchParent(value);
+            AVLNode parent = searchParent(value);
             if (targetNode.left == null && targetNode.right == null) {
                 // 如果要删除的节点是叶子节点 则判断targetNode 是 parent的 左节点还是右节点
                 if (parent.left != null && parent.left.value == value) {
@@ -56,7 +64,7 @@ class BinarySortTreeU {
                 // 删除的目标节点有两颗子树
 
                 // 从右边找最小的
-                BinaryNode node = targetNode.right;
+                AVLNode node = targetNode.right;
                 // 向左边递归查找 一直找到最小值
                 while (node.left != null) {
                     node = node.left;
@@ -100,7 +108,7 @@ class BinarySortTreeU {
         }
     }
 
-    public BinaryNode search(int value) {
+    public AVLNode search(int value) {
         if (root == null) {
             return null;
         } else {
@@ -108,7 +116,7 @@ class BinarySortTreeU {
         }
     }
 
-    public BinaryNode searchParent(int value) {
+    public AVLNode searchParent(int value) {
         if (root == null) {
             return null;
         } else {
@@ -125,16 +133,66 @@ class BinarySortTreeU {
     }
 }
 
-class BinaryNode {
+class AVLNode {
     int value;
-    BinaryNode left;
-    BinaryNode right;
+    AVLNode left;
+    AVLNode right;
 
-    public BinaryNode(int value) {
+    public AVLNode(int value) {
         this.value = value;
     }
 
-    public void add(BinaryNode node) {
+    // 左旋转
+    public void leftRotate() {
+        // 创建新的节点，以当前根节点的值
+        AVLNode newNode = new AVLNode(value);
+        // 把新的节点的左子树设置成当前节点的左子树
+        newNode.left = left;
+        // 把新节点的右子树设置成当前节点的右子树的左子树
+        newNode.right = right.left;
+        // 把当前节点的值替换成右子节点的值
+        value = right.value;
+        // 把当前节点的右子树设置成当前节点右子树的右子树
+        right = right.right;
+        // 把当前节点的左子树设置成新的节点
+        left = newNode;
+    }
+
+    // 右旋转
+    public void rightRotate() {
+        // 创建新的节点，以当前根节点的值
+        AVLNode newNode = new AVLNode(value);
+        // 把新节点的右子树设置成当前节点的右子树
+        newNode.right = right;
+        // 把新节点的左子树设置成当前节点的左子树的右子树
+        newNode.left = left.right;
+        // 把当前节点的值替换成左子节点的值
+        value = left.value;
+        // 把当前节点的左子树设置成当前节点左子树的左子树
+        left = left.left;
+        // 把当前节点的右子树设置成新的节点
+        right = newNode;
+    }
+
+    public int leftHeight() {
+        if (left != null) {
+            return left.height();
+        }
+        return 0;
+    }
+
+    public int rightHeight() {
+        if (right != null) {
+            return right.height();
+        }
+        return 0;
+    }
+
+    public int height() {
+        return Math.max(left == null ? 0 : left.height(), right == null ? 0 : right.height()) + 1;
+    }
+
+    public void add(AVLNode node) {
         if (node == null) {
             return;
         }
@@ -151,9 +209,17 @@ class BinaryNode {
                 this.right.add(node);
             }
         }
+        // 当添加完一个节点后，如果 右子树的高度 - 左子树的高度 > 1 则左旋转
+        if (rightHeight() - leftHeight() > 1) {
+            leftRotate();
+        }
+        // 当添加完一个节点后，如果 左子树的高度 - 右子树的高度 > 1 则右旋转
+        if (leftHeight() - rightHeight() > 1) {
+            rightRotate();
+        }
     }
 
-    public BinaryNode search(int value) {
+    public AVLNode search(int value) {
         if (this.value == value) {
             return this;
         }
@@ -169,7 +235,7 @@ class BinaryNode {
         return null;
     }
 
-    public BinaryNode searchParent(int value) {
+    public AVLNode searchParent(int value) {
         if ((this.left != null && this.left.value == value)
                 || (this.right != null && this.right.value == value)) {
             return this;
@@ -197,7 +263,7 @@ class BinaryNode {
 
     @Override
     public String toString() {
-        return "BinaryNode{" +
+        return "AVLNode{" +
                 "value=" + value +
                 '}';
     }
