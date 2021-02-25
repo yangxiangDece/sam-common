ca57c9e254d506b0b7673f9f88891eb26142303f
yangxiang <yangxiang@chuangjialive.com>
2021-02-25 10:59:47 +0800

    region

--- src/test/java/com/yang/spring/Test.java
+++ src/test/java/com/yang/spring/Test.java
@@ -3,8 +3,5 @@ package com.yang.spring;
 public class Test {
 
     public static void main(String[] args) {
-        Float a = 0.01f;
-        double b = a;
-        System.out.println(Double.parseDouble(String.valueOf(a)));
     }
 }
