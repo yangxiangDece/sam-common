21e6e0972baea54079e158bea5ba37c9939bff30
yangxiang <yangxiang@chuangjialive.com>
2021-03-01 11:35:34 +0800

    region

--- src/main/java/com/yang/concurrent/CountDownLatchTest.java
+++ src/main/java/com/yang/concurrent/CountDownLatchTest.java
@@ -6,7 +6,7 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchTest {
 
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(4);
+        CountDownLatch latch = new CountDownLatch(5);
         for (int i = 0; i < latch.getCount(); i++) {
             new Thread(new MyThread(latch), "player" + i).start();
         }
@@ -26,7 +26,7 @@ public class CountDownLatchTest {
         public void run() {
             try {
                 Random rand = new Random();
-                int randomNum = rand.nextInt((3000 - 1000) + 1) + 1000;// 产生1000到3000之间的随机整数
+                int randomNum = rand.nextInt(5000) + 1000;
                 Thread.sleep(randomNum);
                 System.out.println(Thread.currentThread().getName() + " 已经准备好了, 所使用的时间为 " + ((double) randomNum / 1000) + "s");
                 countDownLatch.countDown();
