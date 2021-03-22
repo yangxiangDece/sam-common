1e6a7efd9edb199d60da17aea75ab29e47ab70a5
yangxiang <yangxiang@chuangjialive.com>
2021-03-22 16:06:00 +0800

    sam

--- src/main/java/com/yang/concurrent/ForkJoinPoolTest.java
+++ src/main/java/com/yang/concurrent/ForkJoinPoolTest.java
@@ -22,7 +22,7 @@ public class ForkJoinPoolTest {
     }
 
     private final static int MAX = 20;
-    private static int[] nums = new int[500];
+    private static int[] nums = new int[20];
     private static Random r = new Random();
 
     static {
---
+++ src/test/java/com/yang/spring/AllTest.java
@@ -0,0 +1,8 @@
+package com.yang.spring;
+
+public class AllTest {
+
+    public static void main(String[] args) {
+
+    }
+}
